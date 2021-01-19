c30be2366f37b7b4ec4f2840e0d9d1a73884b89f
lyz52807 <59039910+lyz52807@users.noreply.github.com>
2021-01-19 18:17:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,5 +11,10 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/vernesong/OpenClash package/openclash
+git clone https://github.com/jerrykuku/lua-maxminddb package/lua-maxmindd
+git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
 ./scripts/feeds update -a
 ./scripts/feeds install -a
